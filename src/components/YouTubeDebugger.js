304c767ad58ad3ed9d4fec99be4e28c9efbad7f0
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-29 22:16:39 +0000

    automatically backed up by learn

--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,14 @@
-// Code YouTubeDebugger Component Here
+import React from 'react'
+
+export default class YouTubeDebugger extends React.Component {
+  constructor() {
+    super()
+    js { errors: [], user: null, settings: { bitrate: 8, video: { resolution: '1080p' } } }
+  }
+
+  handleClick = (event) => {
+    this.setState({
+      bitrate: 12
+    })
+  }
+}
