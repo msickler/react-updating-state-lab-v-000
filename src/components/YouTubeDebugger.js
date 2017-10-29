6f5f1e3d5b50ae983354af9fcfe274738474ee70
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-29 22:18:38 +0000

    automatically backed up by learn

--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -3,12 +3,26 @@ import React from 'react'
 export default class YouTubeDebugger extends React.Component {
   constructor() {
     super()
-    js { errors: [], user: null, settings: { bitrate: 8, video: { resolution: '1080p' } } }
+    this.state = {
+      errors: [],
+      user: null,
+      settings: {
+        bitrate: 8,
+        video: {
+         resolution: '1080p' }
+       }
+     }
   }
 
   handleClick = (event) => {
     this.setState({
       bitrate: 12
     })
+  }
+
+  render() {
+    return (
+
+    )
   }
 }
